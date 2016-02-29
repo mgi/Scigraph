79bb810e0f0051b49fa2bc94ce748be0080e8712
Manuel Giraud <manuel@ledu-giraud.fr>
2016-02-29 16:14:42 +0100

    just call them equation

--- equation.lisp
+++ equation.lisp
@@ -1,13 +1,13 @@
 (in-package :clim3-scigraph)
 
-(defclass x-equation (graph)
+(defclass equation (graph)
   ((%eqn :initarg :equation :reader equation)
    (%fun :initarg :func :reader func)
    (%min :initform 0 :initarg :min :reader minimum)
    (%max :initform 100 :initarg :max :reader maximum)
    (%increment :initform 1 :initarg :increment :reader increment)))
 
-(defmethod initialize-instance :after ((object x-equation) &key &allow-other-keys)
+(defmethod initialize-instance :after ((object equation) &key &allow-other-keys)
   (with-accessors ((func func)
                    (min minimum)
                    (max maximum)
@@ -16,8 +16,8 @@
     (setf data (loop for x from min to max by increment
                      collect (cons x (funcall func x))))))
 
-(defun make-x-equation (lambda-equation min max increment color thickness)
-  (make-instance 'x-equation
+(defun make-equation (lambda-equation min max increment color thickness)
+  (make-instance 'equation
                  :equation lambda-equation
                  :func (compile nil lambda-equation)
                  :min min
@@ -25,6 +25,3 @@
                  :increment increment
                  :color color
                  :thickness thickness))
-
-(defclass xy-equation (graph)
-  ((%eqn :initform #'identity :initarg :equation :accessor equation)))
